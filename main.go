e4073a3486b6a7b42261dbb233896f96236a61ca
Gookuruto <gokuruto1234@gmail.com>
2022-07-23 15:41:39 +0200

    first commit

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"os"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func main() {
-	log_file = os.OpenFile("Log.json",os.O_APPEND | os.O_CREATE | os.O_WRONLY,0644)
+	log_file, err := os.OpenFile("Log.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic("Error when opening log file.")
+	}
 	sugar_logger := logInit(false, log_file)
 
 	//Pass sugar to other objects
@@ -16,24 +20,24 @@ func main() {
 
 func logInit(d bool, f *os.File) *zap.SugaredLogger {
 
-    pe := zap.NewProductionEncoderConfig()
+	pe := zap.NewProductionEncoderConfig()
 
-    fileEncoder := zapcore.NewJSONEncoder(pe)
+	fileEncoder := zapcore.NewJSONEncoder(pe)
 
-    pe.EncodeTime = zapcore.ISO8601TimeEncoder # The encoder can be customized for each output
-    consoleEncoder := zapcore.NewConsoleEncoder(pe)
+	pe.EncodeTime = zapcore.ISO8601TimeEncoder // The encoder can be customized for each output
+	consoleEncoder := zapcore.NewConsoleEncoder(pe)
 
-    level := zap.ErrorLevel
-    if d {
-        level = zap.DebugLevel
-    }
+	level := zap.ErrorLevel
+	if d {
+		level = zap.DebugLevel
+	}
 
-    core := zapcore.NewTee(
-        zapcore.NewCore(fileEncoder, zapcore.AddSync(f), level),
-        zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), level),
-    )
+	core := zapcore.NewTee(
+		zapcore.NewCore(fileEncoder, zapcore.AddSync(f), level),
+		zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), level),
+	)
 
-    l := zap.New(core) // Creating the logger
+	l := zap.New(core) // Creating the logger
 
-    return l.Sugar()
+	return l.Sugar()
 }
