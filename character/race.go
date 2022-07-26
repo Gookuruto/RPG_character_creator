880d9a6cc91e27036ee39f2a214c636823472d58
Gookuruto <gokuruto1234@gmail.com>
2022-07-26 17:06:09 +0200

    test fix

--- character/race.go
+++ character/race.go
@@ -20,11 +20,13 @@ func CustomRace(name string, modifiers Stats, traits []Feature, languages []stri
 func CreateDefinedRace(raceName string) (*Race, error) {
 	switch raceName {
 	case "Dwarf":
+		return Dwarf(), nil
 	case "Human":
-	case "Orc"
+	case "Orc":
+		return Orc(), nil
 	case "Elven":
 	case "Half-orc":
-	case "Half-elven"
+	case "Half-elven":
 	default:
 		return nil, fmt.Errorf("Race %s not exist", raceName)
 	}
@@ -35,20 +37,22 @@ func Dwarf() *Race {
 	var moifiers Stats = Stats{Strength: 2, Constitution: 3}
 	features := make([]Feature, 0)
 	features = append(features, Feature{Name: "Dwarven skin", Description: "Gives +5 defence against physical attacks."})
-	race := &Race{Modifiers: moifiers, Traits: features}
+	race := &Race{Name: "Dwarf", Modifiers: moifiers, Traits: features,Languages: make([]string, 0)}
 
 	return race
 }
 
-func Orc() *Race{
-	//modifier:= Stats{Strength: 3, Constitution: 2}
-	return nil
+func Orc() *Race {
+	modifier := Stats{Strength: 3, Constitution: 2}
+	features := make([]Feature, 0)
+	features = append(features, Feature{Name: "Brutal Strength", Description: "Gives +10 to attack damage with melee weapons."})
+	return &Race{Modifiers: modifier, Traits: features}
 }
 
 func (race *Race) SaveToJson(path string) {
 	json, err := json.MarshalIndent(&race, "", " ")
 	if err != nil {
-		panic("Cannot marsha; race to json.")
+		panic("Cannot marshall race to json.")
 	}
 	_ = ioutil.WriteFile(path, json, 0644)
 }
